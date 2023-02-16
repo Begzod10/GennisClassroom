7d0534bc67166ace263c009e169beee262b73350
Begzod10 <rimefara22@gmail.com>
2023-02-17 00:10:17 +0500

    dawdawd

--- backend/basics/views.py
+++ backend/basics/views.py
@@ -41,7 +41,7 @@ def login():
                 {
                     "id": 1,
                     "group_name": "A1",
-                    "group_subject": "English"
+                    "group_subject": "Ingliz tili"
                 },
                 {
                     "id": 2,
@@ -51,9 +51,9 @@ def login():
                 {
                     "id": 3,
                     "group_name": "biologiya",
-                    "group_subject": "biologiya"
+                    "group_subject": "Biologiya"
                 }
-            ], 
+            ],
             "photo_profile": "",
         },
         {
@@ -70,7 +70,7 @@ def login():
                 {
                     "id": 3,
                     "group_name": "B1",
-                    "group_subject": "English"
+                    "group_subject": "Ingliz tili"
                 }
             ],
             "photo_profile": "",
@@ -322,7 +322,7 @@ def login():
                 {
                     "id": 10,
                     "group_name": "Tarix",
-                    "subject_id": 2,
+
                     "group_subject": "Tarix"
                 }
             ],
@@ -341,8 +341,8 @@ def login():
                 {
                     "id": 11,
                     "group_name": "ing",
-                    "subject_id": 2,
-                    "group_subject": "ing"
+
+                    "group_subject": "Ingliz tili"
                 }
             ],
             "photo_profile": "",
@@ -357,7 +357,6 @@ def login():
                 id = user["id"]
                 nik = user["username"]
                 name = user["name"]
-                print(user['password'])
                 platform_password = str(user["password"])
                 surname = user["surname"]
                 teacher_id = user["teacher"]
@@ -377,8 +376,8 @@ def login():
                             for gr in user['group']:
                                 exist_group = Group.query.filter(Group.platform_id == gr['id']).first()
                                 if not exist_group:
-                                    group_add = Group(platform_id=gr['id'], name=gr['group_name'],
-                                                      subject_id=gr['subject_id'])
+                                    subject = Subject.query.filter(Subject.name == gr['group_subject']).first()
+                                    group_add = Group(platform_id=gr['id'], name=gr['group_name'], subject_id=subject.id)
                                     db.session.add(group_add)
                                     db.session.commit()
                                     if group_add not in student.groups:
@@ -394,11 +393,11 @@ def login():
                             db.session.add(teacher)
                             db.session.commit()
                             for gr in user['group']:
-                                print(True, False)
                                 exist_group = Group.query.filter(Group.platform_id == gr['id']).first()
                                 if not exist_group:
+                                    subject = Subject.query.filter(Subject.name == gr['group_subject']).first()
                                     group_add = Group(platform_id=gr['id'], name=gr['group_name'],
-                                                      subject_id=gr['subject_id'])
+                                                      subject_id=subject.id)
                                     db.session.add(group_add)
                                     db.session.commit()
                                     if group_add not in teacher.groups:
