3db0cc263e7e75de23a64b35e33ec7fbc445447b
shahzodsobirov <shahzodsobirov246@gmail.com>
2023-02-17 00:32:43 +0500

    222\\

--- backend/teacher/views.py
+++ backend/teacher/views.py
@@ -12,9 +12,15 @@ def teacher_groups():
     return render_template('teacher groups/choose course.html', teacher=teacher)
 
 
-@app.route('/students<int:group_id>')
+@app.route('/students<int:group_id>', methods=['POST', 'GET'])
 def students(group_id):
     group = Group.query.filter(Group.id == group_id).first()
-    print(group)
-
-    return render_template('teacher groups/group students.html', group=group)
+    levels = SubjectLevel.query.filter(SubjectLevel.subject_id == group.subject_id).all()
+    if request.method == "POST":
+        level = request.form.get("level")
+        Group.query.filter(Group.id == group_id).update({
+            "course_id": level
+        })
+        db.session.commit()
+        return redirect(url_for('students', group_id=group_id))
+    return render_template('teacher groups/group students.html', group=group, levels=levels, group_id=group_id)
